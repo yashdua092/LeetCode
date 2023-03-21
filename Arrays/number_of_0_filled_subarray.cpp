512368e5e25baaaa517facf8ab8a2bb0af923272
yashdua092 <yashdua092@gmail.com>
2023-03-21 22:53:16 +0530

    Create number_of_0_filled_subarray.cpp

---
+++ Arrays/number_of_0_filled_subarray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        // if zeros continuous
+        // 1st zero -> 1
+        // 2nd zero -> 1 + 1
+        // 3rd zero -> 1 + 1 + 1 and so on
+
+        int zeros = 0;
+        long long ans = 0;
+        for(int itr : nums){
+            if(itr == 0){
+                zeros += 1;
+                ans += zeros;
+            }
+            else zeros = 0;
+        }
+
+        return ans;
+    }
+};
